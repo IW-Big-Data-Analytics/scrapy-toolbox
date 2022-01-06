bd20f89f9ae1b015d17424f7e69b1280d6016461
Simon Perschel <simon.perschel@t-online.de>
2022-01-06 15:43:53 +0100

    initial commit: add feature for recursive item to model mapping

--- scrapy_toolbox/mapper.py
+++ scrapy_toolbox/mapper.py
@@ -1,5 +1,8 @@
-from inspect import getmembers, isclass # Get all classes from a *.py-script
-from sqlalchemy.inspection import inspect # Get PKs from model-class
+from inspect import getmembers, isclass  # Get all classes from a *.py-script
+from sqlalchemy.inspection import inspect  # Get PKs from model-class
+from scrapy import Item
+from sqlalchemy.orm.decl_api import DeclarativeMeta
+
 
 class ItemsModelMapper:
     # For each Item there has to be a corrisponding databaseobject that extends scrapy_toolbox.database.DeclarativeBase
@@ -9,19 +12,24 @@ class ItemsModelMapper:
     def __init__(self, items, model):
         self.items = items
         self.model = model
-        self.model_col = {cls_name + "Item" : cls_obj for cls_name, cls_obj in
-                          getmembers(self.model) if isclass(cls_obj)}  # "XYItem" : XY.__class_
+        self.model_col = {
+            cls_name + "Item": cls_obj for cls_name, cls_obj in
+            getmembers(self.model) if isinstance(cls_obj, DeclarativeMeta) and not cls_name == "Base"
+        }  # "XYItem": XY.__class_
 
-    def map_to_model(self, item, sess):
-        model_class = self.model_col[item.__class__.__name__] # get model for item name
+    def map_to_model(self, item: Item, sess) -> DeclarativeMeta:
+        for key in item:
+            if isinstance(item[key], Item):
+                item[key] = self.map_to_model(item[key], sess)
+        model_class: DeclarativeMeta = self.model_col[item.__class__.__name__]  # get model for item name
         primary_keys = [key.name for key in inspect(model_class).primary_key]
         if not set(primary_keys).issubset(set(list(item.keys()))):
-            item = model_class(**{i:item[i] for i in item})
+            item = model_class(**{i: item[i] for i in item})
             return item
-        filter_param = {item_id:item[item_id] for item_id in primary_keys}
+        filter_param = {item_id: item[item_id] for item_id in primary_keys}
         item_by_id = sess.query(model_class).filter_by(**filter_param).first()
         if item_by_id is None:
-            item = model_class(**{i:item[i] for i in item})
+            item = model_class(**{i: item[i] for i in item})
         else:
             item = item_by_id
         return item
