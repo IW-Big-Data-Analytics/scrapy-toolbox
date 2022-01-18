5250050d5c18157ed4e3ef94b3163f62095a1f85
Simon Perschel <49566229+siper102@users.noreply.github.com>
2022-01-18 11:37:37 +0100

    modify commands

--- scrapy_toolbox/command_line.py
+++ scrapy_toolbox/command_line.py
@@ -1,9 +1,9 @@
 import subprocess
-import sys
 import click
 import string
 import os
 from pathlib import Path
+from typing import Tuple
 
 
 templates_all = ["models.py.tmpl",
@@ -11,9 +11,10 @@ templates_all = ["models.py.tmpl",
                  "settings.py.tmpl"]
 
 location_of_file: Path = Path(__file__)
+path_templates = location_of_file.parent.parent.joinpath("templates")
 
 
-def _render_template(file, **kwargs):
+def _render_template(file, **kwargs) -> Tuple[str, str]:
     with open(file, "r") as f:
         raw = f.read()
     content = string.Template(raw).substitute(**kwargs)
@@ -36,17 +37,16 @@ def check_output(spider):
         subprocess.check_output(["scrapy", "crawl", spider, "-a process_errors=True"])
 
 @cli.command()
-@click.argument("projectname")
+@click.argument("projectname", required=True)
 def startproject(projectname: str):
     """
     Extends the scrapy startproject command by adding specific files and lines for scrapy-toolbox.
     :param projectname: Name of the project.
     """
-    path_templates = location_of_file.parent.parent.joinpath("templates")
     if not path_templates.exists():
         raise FileNotFoundError("Template File is missing.")
     Projectname = projectname.capitalize().replace("_", "").replace("-", "")
-    subprocess.check_output(f"scrapy startproject {projectname} > /dev/null")
+    os.system(f"scrapy startproject {projectname} > /dev/null")
     for template_file in templates_all:
         template_absolute_path = path_templates.joinpath(template_file)
         if not template_absolute_path.exists():
@@ -58,7 +58,7 @@ def startproject(projectname: str):
         with open(f"{projectname}/{projectname}/{path}", "w") as f:
             f.write(content)
     click.echo(f"""
-    New Scrapy project 'hi' created in: {os.path.abspath(os.curdir) + "/" + projectname}
+    New Scrapy project '{projectname}' created in: {os.curdir + "/" + projectname}
     You can start your first spider with:
     cd {projectname}
     scrapy-toolbox genspider example example.com
@@ -66,21 +66,26 @@ def startproject(projectname: str):
 
 
 @cli.command()
-@click.argument("spidername")
-@click.argument("domain")
+@click.argument("spidername", nargs=1)
+@click.argument("domain", default="Example.com")
 def genspider(spidername: str, domain: str):
     """
     Extends scrapy genspider command and add imports and metaclass to spider.
     :param spidername: Name of the spider.
     :param domain: Domain for spider.
     """
-    assert "scrapy.cfg" in os.listdir(), "not a scrapy directory. Use scrapy-toolbox startproject to create one."
+    assert "scrapy.cfg" in os.listdir(), "not a scrapy directory. Use scrapy-toolbox startproject to create one " \
+                                         "or navigate to project directory."
     os.system(f"scrapy genspider {spidername} {domain}")
-    path_file = os.path.abspath(__file__).replace("/commands/commands.py", "")
+    if not path_templates.exists():
+        raise FileNotFoundError("Template File is missing.")
+    spider_template = path_templates.joinpath("toolbox_spider.py.tmpl")
+    if not spider_template.exists():
+        raise FileNotFoundError("Spider Template File is missing.")
     classname = spidername.capitalize().replace("-", "").replace("_", "")
     project_name = os.path.basename(os.path.abspath(os.curdir))
     content, path = _render_template(
-        file=f"{path_file}/templates/toolbox_spider.py.tmpl",
+        file=path_templates.joinpath("toolbox_spider.py.tmpl"),
         name=spidername,
         domain=domain,
         classname=classname
