de42f2bb6c0ae03f9269089179ea28b91724aa89
Simon Perschel <49566229+siper102@users.noreply.github.com>
2022-02-16 16:09:19 +0100

    test: hometown for two persons get only inserted once

--- tests/test_database_pipeline.py
+++ tests/test_database_pipeline.py
@@ -58,6 +58,55 @@ class TestDatabasePipeline():
             assert stored_person.hometown.name == 'Weyhe'
             assert stored_person.hometown.population == 15000.0
 
+
+    @pytest.mark.usefixtures('setup_database', 'connection')
+    def test_unique_inserted_once(setup_database, connection):
+        name_1: Final[Name] = Name(name='Bjarne')
+        name_2: Final[Name] = Name(name="Simon")
+        hometown: Final[Hometown] = Hometown(name='Weyhe', population=15000.0)
+        person_1: Final[Person] = Person(
+            weight=80.5,
+            height=185.0,
+            shirt_color='red',
+            name=name_1,
+            hometown=hometown
+        )
+        person_2: Final[Person] = Person(
+            weight=76,
+            height=182.0,
+            shirt_color='white',
+            name=name_2,
+            hometown=hometown
+        )
+
+        settings: Final[dict] = {
+            'DATABASE_DEV': {
+                'drivername': 'mysql+pymysql',
+                'username': 'root',
+                'password': 'admin',
+                'database': 'test',
+                'host': 'localhost',
+                'port': '3306'
+            }
+        }
+
+        person_items: Final[ModuleType] = importlib.import_module('tests.test_resources.items.person_items')
+        person_model: Final[ModuleType] = importlib.import_module('tests.test_resources.models.person_model')
+
+        db_pipe: Final[DatabasePipeline] = DatabasePipeline(settings, items=person_items, model=person_model)
+        db_pipe.insert_into_db([person_1, person_2])
+
+        with Session(bind=connection) as session:
+            stored_persons = session.execute(
+                select(Person)
+            ).scalars().all()
+
+            stored_person_1 = stored_persons[0]
+            stored_person_2 = stored_persons[1]
+
+            assert (stored_person_1.hometown == stored_person_2.hometown)
+
+
     # TODO: Fails
     @pytest.mark.usefixtures('setup_database', 'connection')
     def test_duplicate_insert(setup_database, connection):
@@ -97,3 +146,6 @@ class TestDatabasePipeline():
             db_pipe.insert_into_db([person_1, person_2])
         except IntegrityError:
             pytest.fail("Duplicate Insert in Database.")
+
+
+
